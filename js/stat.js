ee7c200355a4e464825e75eefe87750249815779
EBuscarov <EBuscarov@gmail.com>
2018-11-30 01:31:11 +0900

    Добавил на главную страницу скрипт вывода статистики. Модифицировал код скрипта для выполнения задачи - учебный проект Холст

---
+++ js/stat.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 90;
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (FONT_GAP) * i, CLOUD_HEIGHT - GAP);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_WIDTH + (FONT_GAP) * i, BAR_HEIGHT + FONT_GAP - GAP - (BAR_HEIGHT * Math.round(times[i])) / maxTime);
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random().toFixed(1) + '';
+    }
+    ctx.fillRect(CLOUD_X + BAR_WIDTH + (FONT_GAP) * i, BAR_HEIGHT + FONT_GAP, BAR_WIDTH, -(BAR_HEIGHT * Math.round(times[i])) / maxTime);
+  }
+};
